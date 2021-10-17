90442bc9de175de43e35d92c2717f7b316993a3f
theflexone <edhavey@null.net>
2021-10-17 00:22:42 -0500

    4 tests complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+class Employee {
+    constructor(firstName, familyName, title, payPerHour) {
+        this.firstName = firstName,
+        this.familyName = familyName,
+        this.title = title,
+        this.payPerHour
+    }
+}
+
+const createEmployeeRecord = (employee) => {
+    return new Employee(...employee);
+}
